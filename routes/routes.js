d21e52c9cc49dd7789fcf4224a1af14cec339f70
Haris-Shahid <harisshahid01@yahoo.com>
2019-05-13 10:21:29 -0700

    procfile added

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ var AuthUser = require('../models/auth');
 module.exports = (app) => {
     app.post('/auth/register', AuthController.RegisterUser)
     app.post('/auth/login', AuthController.LoginUser)
-    app.get('/auth/profile', AuthController.AuthUserProfile)
+    // app.get('/auth/profile', AuthController.AuthUserProfile)
     app.get('/auth/profile', (req, res) => {
         AuthUser.find({}).then(user => {
             res.send(user)
